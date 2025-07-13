9eaee20c86b3ce8158f51701372f293e6bd8aa86
hitesh-s0lanki <hiteshsolanki4623@gmail.com>
2025-07-13 16:05:13 +0530

    -- build error solve

--- book-my-show-fe/src/app/(public)/_components/hero-carosoul.tsx
+++ book-my-show-fe/src/app/(public)/_components/hero-carosoul.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import {
   Carousel,
   CarouselContent,
@@ -9,10 +7,10 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { Film, Music, Star, Trophy } from "lucide-react";
 import Image from "next/image";
 import { useRef } from "react";
 import Autoplay from "embla-carousel-autoplay";
+import { cn } from "@/lib/utils";
 
 type Props = {};
 
@@ -49,15 +47,18 @@ const HeroCarousel = ({}: Props) => {
         plugins={[plugin.current]}
         className="w-full"
         onMouseEnter={plugin.current.stop}
-        onMouseLeave={plugin.current.reset}
-      >
+        onMouseLeave={plugin.current.reset}>
         <CarouselContent defaultValue={2}>
-          {featuredItems.map((item) => (
+          {featuredItems.map((item, index) => (
             <CarouselItem
               key={item.id}
-              className="max-w-[1280px] max-h-[400px] px-5  rounded-lg"
-            >
-              <div className="relative w-full ">
+              className="max-w-[1280px] max-h-[400px] px-5  rounded-lg">
+              <div
+                className={cn(
+                  "relative w-full",
+                  index == 0 && "pl-20",
+                  index == featuredItems.length - 1 && "pr-20"
+                )}>
                 <Image
                   src={item.image}
                   alt={item.title}
@@ -65,36 +66,6 @@ const HeroCarousel = ({}: Props) => {
                   width={500}
                   className=" w-full rounded-lg object-contain object-center"
                 />
-                {/* <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
-                  <div className="text-center text-white max-w-2xl px-4">
-                    <h1 className="text-4xl md:text-6xl font-bold mb-4">
-                      {item.title}
-                    </h1>
-                    <Badge className="mb-4 text-lg px-4 py-2">
-                      {item.type === "movie" && (
-                        <Film className="w-4 h-4 mr-2" />
-                      )}
-                      {item.type === "event" && (
-                        <Music className="w-4 h-4 mr-2" />
-                      )}
-                      {item.type === "sports" && (
-                        <Trophy className="w-4 h-4 mr-2" />
-                      )}
-                      {item.type.toUpperCase()}
-                    </Badge>
-                    {item.rating && (
-                      <div className="flex items-center justify-center mb-4">
-                        <Star className="w-5 h-5 text-yellow-400 mr-1" />
-                        <span className="text-lg font-semibold">
-                          {item.rating}
-                        </span>
-                      </div>
-                    )}
-                    <Button size="lg" className="bg-red-600 hover:bg-red-700">
-                      Book Now
-                    </Button>
-                  </div>
-                </div> */}
               </div>
             </CarouselItem>
           ))}
