69069808ac6ed556ddb3ad4803655b30ab9aa5eb
hitesh-s0lanki-striq <hitesh.solanki@strique.io>
2025-09-03 17:26:16 +0530

    feat: movies model completed with logger service

--- book-my-show-be/src/main/java/com/bookmyshow/interfaces/MovieServiceInter.java
+++ book-my-show-be/src/main/java/com/bookmyshow/interfaces/MovieServiceInter.java
@@ -1,5 +1,18 @@
 package com.bookmyshow.interfaces;
 
+import org.springframework.http.ResponseEntity;
+
+import com.bookmyshow.models.Movie;
+
 public interface MovieServiceInter {
 
+    public ResponseEntity<?> getAllMovies();
+
+    public ResponseEntity<?> getMovieById(String movieId);
+
+    public ResponseEntity<?> createMovie(Movie movie);
+
+    public ResponseEntity<?> updateMovie(String movieId, Movie movie);
+
+    public ResponseEntity<?> deleteMovie(String movieId);
 }
