74fec4b39f8d1a96c553ca874a43dfb48d726cb1
hitesh-s0lanki-striq <hitesh.solanki@strique.io>
2025-09-04 17:33:48 +0530

    feat: events data completed

--- data-dump/index.js
+++ data-dump/index.js
@@ -6,15 +6,17 @@ async function fetchMoviesUpload() {
     const movies = fs.readFileSync("./movies.json");
     const moviesData = JSON.parse(movies);
 
-    moviesData.forEach(async (movie) => {
-      const response = await axios.post(
-        "http://localhost:8080/api/movies",
-        movie
-      );
-      console.log(
-        `Uploaded movie: ${movie.title}, Response: ${response.status}`
-      );
-    });
+    await Promise.all(
+      moviesData.map(async (movie) => {
+        const response = await axios.post(
+          "http://localhost:8080/api/movies",
+          movie
+        );
+        console.log(
+          `Uploaded movie: ${movie.title}, Response: ${response.status}`
+        );
+      })
+    );
 
     console.log("Movies data fetched and saved to movies.json");
   } catch (error) {
@@ -22,4 +24,30 @@ async function fetchMoviesUpload() {
   }
 }
 
-fetchMoviesUpload();
+async function fetchEventsUpload() {
+  try {
+    const events = fs.readFileSync("./events.json");
+    const eventsData = JSON.parse(events);
+
+    await Promise.all(
+      eventsData.map(async (event) => {
+        const response = await axios.post(
+          "http://localhost:8080/api/events",
+          `Uploaded event: ${event.title}, Response: ${response.status}`
+        );
+        console.log(
+          `Uploaded event: ${event.name}, Response: ${response.status}`
+        );
+      })
+    );
+
+    console.log("Events data fetched and saved to events.json");
+  } catch (error) {
+    console.error("Error fetching events data:", error);
+  }
+}
+
+// Uncomment the function you want to run
+fetchEventsUpload();
+
+// fetchMoviesUpload();
